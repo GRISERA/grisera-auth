f522e43890f6a5c91947197482b9784110f361a0
kamil <kamil.czepiell@gmail.com>
2024-12-08 20:30:10 +0100

    userId as string

--- models/Permission.js
+++ models/Permission.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const PermissionSchema = new mongoose.Schema({
     userId: {
-        type: mongoose.Schema.Types.ObjectId,
+        type: String,
         ref: 'User',
         required: true,
     },
